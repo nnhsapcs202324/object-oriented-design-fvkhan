534e30f97c40e5c772816d21b8f223dae2f24610
Fatimah Khan <fvkhan@stu.naperville203.org>
2024-02-13 08:30:11 -0600

    working on betterrectangle

---
+++ BetterRectangle.java
@@ -0,0 +1,21 @@
+import java.awt.Rectangle;
+/**
+ * computes area and perimeter of rectangle
+ *
+ * @fvkhan
+ * @13feb2024
+ */
+public class BetterRectangle extends Rectangle
+{
+    public BetterRectangle(int height, int width, int x, int y)
+    {
+        super.setSize(height, width);
+        super.setLocation(x, y);
+    }
+    
+    public int getArea(BetterRectangle rectangle)
+    {
+        
+    }
+}
+
