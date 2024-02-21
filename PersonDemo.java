c4e18caab5330fe4f181b1bd8c10a8982da0a9f0
Fatimah Khan <fvkhan@stu.naperville203.org>
2024-02-21 09:40:35 -0600

    working on persondemo

--- PersonDemo.java
+++ PersonDemo.java
@@ -1,33 +1,50 @@
-
-/**
- * Write a description of class PersonDemo here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+import java.util.Scanner;
+import java.util.ArrayList;
 public class PersonDemo
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class PersonDemo
-     */
-    public PersonDemo()
+    public static void main (String args[])
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        ArrayList<Person> list = new ArrayList<Person>();
+        Scanner s = new Scanner(System.in);
+        for (int i = 0; i<3; i++)
+        {
+            System.out.print("Enter a name: ");
+            String name = s.next();
+            System.out.print("Enter an age: ");
+            int age = s.nextInt();
+            Person p = new Person(name,age);
+            list.add(p);
+        }
+        Person first = list.get(0);
+        Person last = list.get(0);
+        for (int i = 1; i < list.size(); i++)
+        {
+            if (first.compareTo(list.get(i))==2)
+            {
+                continue;
+            }
+            else if (first.compareTo(list.get(i))>0)
+            {
+                first = list.get(i);
+            }
+        }
+       
+        for (int i = 1; i < list.size(); i++)
+        {
+            System.out.println(last.compareTo(list.get(i)));
+            if (last.compareTo(list.get(i))==2)
+            {
+                last = list.get(i);
+            }
+            else if (last.compareTo(list.get(i))<0)
+            {
+                continue;
+            }
+        }
+       
+        System.out.println(first.getName());
+        System.out.println(first.getAge());
+        System.out.println(last.getName());
+        System.out.println(last.getAge());
     }
 }
