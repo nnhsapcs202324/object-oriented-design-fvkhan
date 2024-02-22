4b0b7a25a53afa2f96f761168e604b1fe69467ce
Fatimah Khan <fvkhan@stu.naperville203.org>
2024-02-22 08:13:44 -0600

    completed button listener part 1

---
+++ ClickListener.java
@@ -0,0 +1,16 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/**
+ * Write a description of class ClickListener here.
+ *
+ * @author fvkhan
+ * @version 22feb2024
+ */
+public class ClickListener implements ActionListener
+{
+    @Override
+    public void actionPerformed(ActionEvent event)
+    {
+        System.out.println("button clicked");
+    }
+}
