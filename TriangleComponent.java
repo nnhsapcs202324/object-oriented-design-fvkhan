ce320b8452c47a0abe180d1e873ee8b31174bdc6
Fatimah Khan <fvkhan@stu.naperville203.org>
2024-02-27 08:29:53 -0600

    working on triangle gui

--- TriangleComponent.java
+++ TriangleComponent.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 public class TriangleComponent extends JComponent
 {
     private static final int MAX_POINTS = 3;
-    private int clickCount;
+    private int clickCount = 1;
     private ArrayList<Point2D> points;
     private Point2D point1;
     private Point2D point2;
@@ -21,81 +21,91 @@ public class TriangleComponent extends JComponent
     private Line2D line2;
     private Line2D line3;
     private double x1;
-    private double x2;
-    private double x3;
     private double y1;
-    private double y2;
-    private double y3;
-    
+
     public TriangleComponent()
     {
         MouseClickListener mouseClickListener = new MouseClickListener();
         this.addMouseListener(mouseClickListener);
     }
-    
+
     public class MouseClickListener implements MouseListener
     {
         @Override
         public void mouseClicked(MouseEvent event)
         {
             points = new ArrayList<Point2D>();
-            while (clickCount < MAX_POINTS)
+            if (clickCount == 1)
             {
-                int num = clickCount + 1;
+                int num = clickCount;
+                System.out.println("Mouse clicked " + num + " times!");
+                int x = event.getX();
+                int y = event.getY();
+                System.out.println("X coordinate: " + x + "; Y coordinate: " + y);
+                Point2D.Double point = new Point2D.Double(x, y);
+                point1 = point;
+                points.add(point);
+                x1 = x;
+                y1 = y;
+                ellipse = new Ellipse2D.Double(x1, y1, 5, 5);
+                repaint();
+                clickCount++;
+            }
+            else if (clickCount == 2)
+            {
+                int num = clickCount;
+                System.out.println("Mouse clicked " + num + " times!");
+                int x = event.getX();
+                int y = event.getY();
+                System.out.println("X coordinate: " + x + "; Y coordinate: " + y);
+                Point2D.Double point = new Point2D.Double(x, y);
+                point2 = point;
+                points.add(point);
+                line1 = new Line2D.Double(point1, point2);
+                repaint();
+                clickCount++;
+            }
+            else if (clickCount == 3)
+            {
+                int num = clickCount;
                 System.out.println("Mouse clicked " + num + " times!");
                 int x = event.getX();
                 int y = event.getY();
                 System.out.println("X coordinate: " + x + "; Y coordinate: " + y);
                 Point2D.Double point = new Point2D.Double(x, y);
                 points.add(point);
+                point3 = point;
+                line2 = new Line2D.Double(point2, point3);
+                line3 = new Line2D.Double(point3, point1);
+                repaint();
                 clickCount++;
-                
-                if (clickCount == 1)
-                {
-                    point1 = points.get(0);
-                    x1 = point1.getX();
-                    y1 = point1.getY();
-                    ellipse = new Ellipse2D.Double(x1, y1, 5, 5);
-                }
-                else if (clickCount == 2)
-                {
-                    point2 = points.get(1);
-                    x2 = point2.getX();
-                    y2 = point2.getY();
-                    line1 = new Line2D.Double(x1, y1, x2, y2);
-                }
-                else if (clickCount == 3)
-                {
-                    point3 = points.get(2);
-                    x3 = point3.getX();
-                    y3 = point3.getY();
-                    line2 = new Line2D.Double(x2, y2, x3, y3);
-                    line3 = new Line2D.Double(x3, y3, x1, y1);
-                }
-                
+            }
+            
+            else if (clickCount == 4)
+            {
+                int num = clickCount;
+                System.out.println("Mouse clicked " + num + " times! Canvas will clear.");
+                clickCount = 1;
+                points.remove(0);
+                points.remove(0);
+                points.remove(0);
                 repaint();
             }
-            if (clickCount == 4)
-            {
-                clickCount = 0;
-                points.remove(0);
-                points.remove(0);
-                points.remove(0);
-            }
+
         }
         
         @Override
         public void mousePressed(MouseEvent event)
         {}
-        
+
         @Override
         public void mouseExited(MouseEvent event)
         {}
-        
+
         @Override
         public void mouseReleased(MouseEvent event)
         {}
-        
+
         @Override
         public void mouseEntered(MouseEvent event)
         {}
@@ -107,9 +117,19 @@ public class TriangleComponent extends JComponent
         super.paintComponent( g );
         Graphics2D g2 = (Graphics2D) g;
         g2.setColor(Color.BLACK);
-        g2.fill(this.ellipse);
-        g2.fill(this.line1);
-        g2.fill(this.line2);
-        g2.fill(this.line3);
+        if (clickCount == 1)
+        {
+            g2.fill(this.ellipse);
+        }
+
+        else if (clickCount == 2)
+        {
+            g2.fill(this.line1);
+        }
+        else if (clickCount == 3)
+        {
+            g2.fill(this.line2);
+            g2.fill(this.line3);
+        }
     }
 }
